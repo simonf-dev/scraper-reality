4eab824e499c522e18828aece34490964142efdd
Simon Foucek <sfoucek@redhat.com>
2023-03-12 12:51:44 +0100

    added folder with unittests

--- src/tests/test_sanity.py
+++ src/tests/test_sanity.py
@@ -1,7 +1,5 @@
-"""
-Here is place for the unittests
-"""
+"""Here is place for the unittests"""
+
 
 def test_example() -> None:
-    """ example unit test, always returns true"""
-    pass
+    """example unit test, always returns true"""
